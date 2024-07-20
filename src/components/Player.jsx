02f497e63ea68c74cd1d756a64a61552511e7672
Awais Madni <awais.madni17@gmail.com>
2024-07-21 02:23:58 +0500

    pass function to state updating function inside handleClick() as it is a correct way to update state based on the previous state value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react"
 
 export default function Player({name, symbol}) {
-    const [isEditing, setIsEditing] =useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick(){
+        setIsEditing((editing) => !editing);
+    }
+
     let playerName = <span className="player-name">{name}</span>;
     if(isEditing){
-        playerName= <input type="text" required />
+        playerName= <input type="text" value={name} required />
     }
+
     return (
         <li>
             <span className="player">
                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={()=> setIsEditing(true)}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
