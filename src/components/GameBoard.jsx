5486ce20f80161bfaf2d6bcdf5d4c03f5437d981
Awais Madni <awais.madni17@gmail.com>
2024-07-23 02:21:08 +0500

    using the state lift concepts we made the active player visible , and also switch the turns and place the symbol on the grid box with the player who is acrive

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,24 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
-    [null, null, null]
+    [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
-        })
+        });
+
+        onSelectSquare();
     }
+
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => (
@@ -32,5 +35,3 @@ export default function GameBoard() {
         </ol>
     );
 }
-
-
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,30 +1,34 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-export default function Player({initialName, symbol}) {
-    const [playerName, setPlayerName]  = useState(initialName);
+export default function Player({ initialName, symbol, isActive }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
-    function handleEditClick(){
+    function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
-    function handleChange(event){
+    function handleChange(event) {
         setPlayerName(event.target.value);
     }
 
     let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = 'Edit';
 
-    if(isEditing){
-        editablePlayerName= <input type="text" required value={playerName} onChange={handleChange} />
+    if (isEditing) {
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        );
+        // btnCaption = 'Save';
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
-               {editablePlayerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
-    )
+    );
 }
