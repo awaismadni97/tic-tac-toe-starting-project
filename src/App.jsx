6de207fd7e053b781803c32b028304eb0aa57c76
Awais Madni <awais.madni17@gmail.com>
2024-07-25 17:02:55 +0500

    Reducing State Management & Identifying Unnecessary State

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,34 @@ import Player from "./components/Player.jsx"
 import GameBoard from "./components/GameBoard.jsx"
 import Log from "./components/Log.jsx";
 
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+
+  return currentPlayer ;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  // const [activePlayer, setActivePlayer] = useState('X');
+
+  const activePlayer = derivedActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
-      let currentPlayer  = 'X';
-     
-      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O';
-      }
+
+     const currentPlayer = derivedActivePlayer(prevTurns);
 
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: currentPlayer }, 
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns
       ]
 
-      return updatedTurns ;
+      return updatedTurns;
 
     });
   }
@@ -32,7 +41,7 @@ function App() {
         <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
         <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
       </ol>
-      <GameBoard onSelectSquare={handleSelectSquare} turns= {gameTurns} />
+      <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
     </div>
     <Log turns={gameTurns} />
   </main>
