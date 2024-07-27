816d8c094709cd1d1c4d09c78a56d501fe25f802
Awais Madni <awais.madni17@gmail.com>
2024-07-27 16:56:51 +0500

    show the winner player name by not lifting the state up

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,10 @@ function derivedActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2'
+  })
   const [gameTurns, setGameTurns] = useState([]);
 
   // const [activePlayer, setActivePlayer] = useState('X');
@@ -47,7 +51,7 @@ function App() {
     if (firstSquareSymbol &&
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -73,16 +77,33 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayer => {
+      return {
+        ...prevPlayer,
+        [symbol]: newName
+      }
+    })
+  }
+
   return <main>
     <div id="game-container">
       <ol id="players" className="highlight-player">
-        <Player initialName='Player 1' symbol='X' isActive={activePlayer === 'X'} />
-        <Player initialName='Player 2' symbol='O' isActive={activePlayer === 'O'} />
+        <Player initialName='Player 1'
+          symbol='X'
+          isActive={activePlayer === 'X'}
+          onChangeName={handlePlayerNameChange}
+        />
+        <Player initialName='Player 2'
+          symbol='O'
+          isActive={activePlayer === 'O'}
+          onChangeName={handlePlayerNameChange}
+        />
       </ol>
       {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
       <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
     </div>
-    <Log turns={gameTurns} />
+    <Log turns={gameTurns} gamePlayer={players} />
   </main>
 }
 
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,8 +1,8 @@
-export default function Log({ turns }) {
+export default function Log({ turns, gamePlayer }) {
     return <ol id="log">
         {turns.map((turn) => (
             <li key={`${turn.square.row}${turn.square.col}`}>
-                {turn.player} selected {turn.square.row}, {turn.square.col}
+                {gamePlayer[turn.player]} selected {turn.square.row}, {turn.square.col}
             </li>
         ))}
     </ol>
